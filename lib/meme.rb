d2518c97f7b6c6b81d5844a3dc9a655db17729b5
Glauber Prado <smade4@gmail.com>
2010-08-28 02:35:04 -0300

    basic oauth support, it works but is untested code, tests to follow

--- lib/meme.rb
+++ lib/meme.rb
@@ -5,11 +5,25 @@ require 'meme/info'
 require 'meme/search'
 require 'meme/request'
 
-require 'oauth/consumer'
-
 DEBUG_URI = nil
 DEBUGGER = nil
+OAUTH = nil
 
 if DEBUGGER
   require 'ruby-debug-base' ; Debugger.start
+end
+
+# this is only required if you want to access private data and in this
+# case you are encouraged to use the oauth mechanism from your app,
+# the one included here is just so we can test it(at the very least
+# you will need new api keys from yahoo for your app). In this case
+# you will need to tweak the request mechanism.
+
+if OAUTH
+require 'external/oauth_util.rb'
+
+o = OauthUtil.new
+o.consumer_key = 'dj0yJmk9c2pjcUZSSFMzaDV3JmQ9WVdrOU5GUjJTSEIzTldVbWNHbzlNVGc0TnpFNU5qTTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD1mMA--';
+o.consumer_secret = '1c50819145aad6074d38aa35dae33f2fb1450126';
+
 end
--- lib/meme/request.rb
+++ lib/meme/request.rb
@@ -2,13 +2,19 @@ module Meme
 
   class Request #:nodoc:
 
-    def self.parse(query)
+    def self.parse(query,oauth=nil)
       url = URI.escape("https://query.yahooapis.com/v1/public/yql?q=#{query}&format=json")
+      if oauth
+        urloauth = URI.escape("https://query.yahooapis.com/v1/yql?#{o.sign(url).query_string}")
+        buffer = open(urloauth).read
+      else
+        buffer = open(url).read
+      end
       if DEBUG_URI
         puts FakeWeb.registered_uri?(:get,url).to_s
         puts url
+        puts urloauth
       end
-      buffer = open(url).read
       JSON.parse(buffer)
     end
 
